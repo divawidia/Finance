278f7cfcea88ada463eab13fea6984b3519bd639
nyomanjyotisa <jyotisa1616@gmail.com>
2021-12-13 10:45:13 +0800

    create add category and expense classes

--- app/src/main/java/com/kelompok8/finance/ui/home/HomeActivity.java
+++ app/src/main/java/com/kelompok8/finance/ui/home/HomeActivity.java
@@ -1,13 +1,36 @@
 package com.kelompok8.finance.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.kelompok8.finance.R;
+import com.kelompok8.finance.ui.stats.StatisticActivity;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+
+        TextView pengeluaranShow = (TextView) findViewById(R.id.pengeluaranShowAll);
+        TextView transaksiShow = (TextView) findViewById(R.id.transaksiShowAll);
+
+        pengeluaranShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, StatisticActivity.class));
+            }
+        });
+        transaksiShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, StatisticActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/kelompok8/finance/ui/stats/StatisticActivity.java
+++ app/src/main/java/com/kelompok8/finance/ui/stats/StatisticActivity.java
@@ -3,6 +3,7 @@ package com.kelompok8.finance.ui.stats;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,13 +13,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.kelompok8.finance.AddPengeluaranActivity;
 import com.kelompok8.finance.R;
+import com.kelompok8.finance.ui.home.HomeActivity;
 
 public class StatisticActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
+        getSupportActionBar().hide();
+
+        Button addBtn = (Button) findViewById(R.id.createButton);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StatisticActivity.this, AddPengeluaranActivity.class));
+            }
+        });
     }
 }
