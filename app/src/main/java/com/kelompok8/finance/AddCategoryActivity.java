5fdff1a0c75ec90446cbde9f1c40ec67926a5708
nyomanjyotisa <jyotisa1616@gmail.com>
2021-12-13 10:45:13 +0800

    create add category and expense classes

---
+++ app/src/main/java/com/kelompok8/finance/AddCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.kelompok8.finance;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddCategoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category);
+    }
+}
